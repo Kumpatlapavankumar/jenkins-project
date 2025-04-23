72130d2d70c4eebec07df491e9201948c0d87301
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-23 12:20:49 +0530

    Add files via upload

---
+++ db/php_docker_table.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3306
+-- Generation Time: Apr 01, 2025 at 06:58 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_docker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `php_docker_table`
+--
+
+CREATE TABLE `php_docker_table1` (
+  `id` int NOT NULL,
+  `title1` varchar(255) NOT NULL,
+  `body1` text NOT NULL,
+  `date_created1` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `php_docker_table`
+--
+
+INSERT INTO `php_docker_table` (`id`, `title`, `body`, `date_created`) VALUES
+(1, 'first post1', 'first message body1', '2025-02-01'),
+(2, 'second post1', 'second post message1', '2025-03-21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
